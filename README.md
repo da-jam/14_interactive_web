aea9e9ed743f4a44ad07f97aab526846ba6f5013
unknown <jammonhou@aol.com>
2021-08-17 14:52:51 -0500

    dropdown

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 14_interactive_web
+
+need to run with
+python -m http.server 8000 --bind 127.0.0.1
+at the index.html level
+
+refreshes with pulldown selection
+but for some reason does things twince??
+
+only first 3 options included
+rest the same just need to be entered.
+
+blank initial page, but populates on selection
+
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,27 +1,42 @@
-// read data
+function init() {
+  //this is a start blank
+}
+init();
 
-d3.json("../samples.json").then( function(data)
-{
+// for dropdown selection
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+// This function is called when a dropdown menu item is selected
+  function optionChanged() {
+    var myselect = document.getElementById("selDataset");
+    ds = myselect.options[myselect.selectedIndex].value;
+
+  console.log(ds);
+
+ // read data
+ d3.json("../samples.json").then( function(data)
+ {
     console.log(data);
+    // console.log(ds);
 
-// bar graph
-// label string
-labelst = [];
-labels = data.samples[1].otu_ids.slice(0,9);
-for (var i=0; i < labels.length; i++){
+ // bar graph
+ // label string
+ labelst = [];
+ labels = data.samples[ds].otu_ids.slice(0,9);
+ for (var i=0; i < labels.length; i++){
   text = "OTU "+labels[i]
 labelst.push(text); 
-};
+ };
 
-// set up values for bar
-values = data.samples[1].sample_values.slice(0,9);
-labels = labelst;
-text = data.samples[1].otu_labels.slice(0,9);
-// console.log(values);
-// console.log(labels);
+ // set up values for bar
+ values = data.samples[ds].sample_values.slice(0,9);
+ labels = labelst;
+ text = data.samples[ds].otu_labels.slice(0,9);
+ // console.log(values);
+ // console.log(labels);
 
-// bar 
-let trace1 = {
+ // bar 
+ let trace1 = {
     x: values,
     y: labels,
     text: text,
@@ -29,23 +44,23 @@ let trace1 = {
     orientation: "h"
   };
 
-let traceData = [trace1]
+ let traceData = [trace1]
 
-let layout = {
+ let layout = {
     title: {text: " Top Ten Samples"},
     xaxis: {title:{text: "Sample Values"}},
     yaxis: {title:{text:"OTU IDS"}}
-}
+  }
 
-Plotly.newPlot("bar", traceData, layout);
-// end of bar graph
+ Plotly.newPlot("bar", traceData, layout);
+ // end of bar graph
 
-//bubble
-valuesb = data.samples[1].sample_values;
-labelsb = data.samples[1].otu_ids;
-textb = data.samples[1].otu_labels;
+ //bubble
+ valuesb = data.samples[ds].sample_values;
+ labelsb = data.samples[ds].otu_ids;
+ textb = data.samples[ds].otu_labels;
 
-let trace2 = {
+ let trace2 = {
     y: valuesb,
     x: labelsb,
     text: textb,
@@ -59,32 +74,41 @@ let trace2 = {
     }
   };
 
-let traceData1 = [trace2]
+ let traceData1 = [trace2]
 
-let layout1 = {
+ let layout1 = {
     title: {text: " All samples with bubble size and color as Sample Values"},
     xaxis: {title:{text: "OTU IDS"}},
     yaxis: {title:{text:"Sample Values"}}
-}
+ }
 
-Plotly.newPlot("bubble", traceData1, layout1);
-// end of bubble
+ Plotly.newPlot("bubble", traceData1, layout1);
+ // end of bubble
 
-// metadata info
-m_id = "ID: "+data.metadata[1].id;
-// console.log(m_id);
-m_age = "Age: "+data.metadata[1].age;
-m_eth = data.metadata[1].ethnicity;
-m_gen = "Gender: "+data.metadata[1].gender;
-m_loc = "Location: "+data.metadata[1].location;
-m_bb = data.metadata[1].bbtype;
-m_wf = data.metadata[1].wfreq;
+ // metadata info
+ 
+ m_id = "ID: "+data.metadata[ds].id;
+ // console.log(m_id);
+ m_age = "Age: "+data.metadata[ds].age;
+ m_eth = "Ethnicity: "+data.metadata[ds].ethnicity;
+ m_gen = "Gender: "+data.metadata[ds].gender;
+ m_loc = "Location: "+data.metadata[ds].location;
+ m_bb = "bbtype: "+data.metadata[ds].bbtype;
+ m_wf = "wfreq: "+data.metadata[ds].wfreq;
 
-document.getElementById("id").innerHTML = m_id;
-document.getElementById("age").innerHTML = m_age;
-document.getElementById("gen").innerHTML = m_gen;
-document.getElementById("loc").innerHTML = m_loc;
+ document.getElementById("id").innerHTML = m_id;
+ document.getElementById("age").innerHTML = m_age;
+ document.getElementById("gen").innerHTML = m_gen;
+ document.getElementById("loc").innerHTML = m_loc;
+ document.getElementById("eth").innerHTML = m_eth;
+ document.getElementById("bb").innerHTML = m_bb;
+ document.getElementById("wf").innerHTML = m_wf;
+ 
+ // end of meta info
 
+ //end of then 
 
-//end of then 
 });
+//end of redraw
+  }
+
